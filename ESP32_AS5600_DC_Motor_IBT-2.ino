149d27da0d19ab8529cd181f17253f976c4fba68
freedom2000 <31324055+f2knpw@users.noreply.github.com>
2022-10-10 12:29:48 +0200

    Update ESP32_AS5600_DC_Motor_IBT-2.ino

--- ESP32_AS5600_DC_Motor_IBT-2.ino
+++ ESP32_AS5600_DC_Motor_IBT-2.ino
@@ -1,3 +1,4 @@
+
 /*run a DC motor with IBT-2 driver and control loop with AS5600 hall encoder*/
 
 
@@ -27,7 +28,7 @@ int rawValue, prevRawValue ;
 
 boolean CW = true;
 int pwmSpeed ;    //PWM duty cycle to command the DC motor speed
-long timeOut;     //used to change setpoint every 30s
+long timeOut;     //used to change setpoint 10s after motor stopped
 
 
 //PID
@@ -40,8 +41,7 @@ double setpoint, input, output; //used by PID lib
 //Specify the links and initial tuning parameters
 double Kp = 60., Ki = 1.2, Kd = 0.;                         //will be modified into the loop
 PID myPID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);  //declare the PID
-#define DEADBAND 400          //if pwm < DEADBAND, it stalls
-#define INERTIA   1155./4096. //inertia of motor when stopping (PID over shoot on sensor position)
+#define DEADBAND 400                                        //id pwm < DEADBAND, it stalls
 
 void setup()
 {
@@ -66,21 +66,23 @@ void setup()
   pinMode(ENABLE_PIN_L, OUTPUT);
   pinMode(ENABLE_PIN_R, OUTPUT);
 
-  timeOut = millis();    //used to change setpoint every 30s
-  pwmSpeed = 0;           //motor stopped at startup (range -2047 to 2047)
+
 
   ledcAttachPin(RPWM_PIN, 0); // assign PWM pins to channels
   ledcAttachPin(LPWM_PIN, 1); // assign PWM pins to channels
   // Initialize channels : ledcSetup(uint8_t channel, uint32_t freq, uint8_t resolution_bits);
   ledcSetup(0, 24000, 11); // 24 kHz PWM, 11-bit resolution (range 0-2047)
   ledcSetup(1, 24000, 11);
+  //start motor driver
+  pwmSpeed = 0;           //motor stopped at startup (range -2047 to 2047)
+  timeOut = millis();
   digitalWrite(ENABLE_PIN_L, HIGH); //enable the half bridges
   digitalWrite(ENABLE_PIN_R, HIGH);
 
-  targetRot = 300.5 ;             //initialize the target number of rotations of the motor shaft (0 at startup)
+  targetRot = 300;                    //initialize the target number of rotations of the motor shaft (0 at startup)
 
   //turn the PID on
-  setpoint =  targetRot-INERTIA; //setpoint is this target number corrected of the INERTIA bias
+  setpoint =  targetRot;              //setpoint is the number of turns on the motor shaft (not the wiper shaft)
   myPID.SetMode(AUTOMATIC);
   myPID.SetOutputLimits(-2047, 2047); //output of the PID is the pwm value. coded on 11 bits and forward/reverse
   myPID.SetSampleTime(1);             //compute the PID every 1ms
@@ -96,17 +98,8 @@ void loop()
   rawValue = as5600.readAngle();
   if (((rawValue - prevRawValue) < -999) && CW) nbRot++ ; //apply hysteresis to detect each full rotation (4095 <--> 0)
   if (((rawValue - prevRawValue) > 999) && !CW) nbRot-- ;
-//    if (prevRawValue != rawValue)
-//    {
-//      Serial.print(rawValue );
-//      Serial.print("\t");
-//      Serial.print(setpoint );
-//      Serial.print("\t");
-//      Serial.print(input );
-//      Serial.print("\t\t");
-//      Serial.println(pwmSpeed);
-//    }
 
+  if (abs(prevRawValue - rawValue) > 10) timeOut = millis();        //rearm timeout if motor is running
   prevRawValue = rawValue; //save the rawValue for next iteration
 
   //PID stuff
@@ -116,10 +109,13 @@ void loop()
 
 
 
-  Kp = 6000;                     // start with an almost fully proportionnal PID
-  Ki = 20.;
-  Kd = 100. ;
- 
+  Kp =  50;                     // start with a fully proportionnal PID
+  Ki = 0.0;
+  if (abs(pwmSpeed) < DEADBAND) //switch to almost full integral, needed to skip "fast" the motor deadband with no motion...
+  {
+    Kp =  5;
+    Ki = 100;
+  }
   myPID.SetTunings(Kp, Ki, Kd);
   myPID.Compute();
 
@@ -128,21 +124,26 @@ void loop()
   pwmSpeed = output;  //use output of the PID to drive the motor
   runMotor();         //will apply direction and pwm value to the driver
 
-  if (millis() - timeOut > 20000)  //change setpoint every 30s
+  if (millis() - timeOut > 3000)      //stop the motor then change setpoint every 6s
   {
-    Serial.print("setpoint, input, sensor ");  //print setpoint and current position reached
-    Serial.print("\t");
-    Serial.print(setpoint );
-    Serial.print("\t");
-    Serial.print(input);
-    Serial.print("\t");
-    Serial.print(rawValue);
-    Serial.print("\t");
-    double fract = double(rawValue) / 4096.;      //compute the decimal part of nbRot using the rawValue of the AS5600
-    if (nbRot >= 0) Serial.println(nbRot + fract);//easy when CW
-    else Serial.println(-( -nbRot + 1. - fract)); //more tricky if CCW
-    setpoint = -setpoint;                         //change the setpoint
-    timeOut = millis();
+    //Serial.print("motor driver stopped ");
+    digitalWrite(ENABLE_PIN_L, LOW);  //disable the half bridges ==> motor fully stopped
+    digitalWrite(ENABLE_PIN_R, LOW);
+    if (millis() - timeOut > 6000)   //restart 3s later
+    {
+      Serial.print("setpoint, input, sensor ");  //print setpoint and current position reached
+      Serial.print("\t");
+      Serial.print(setpoint );
+      Serial.print("\t");
+      Serial.print(input);
+      Serial.print("\t");
+      Serial.println(rawValue);
+
+      setpoint = -setpoint;             //change the setpoint
+      timeOut = millis();
+      digitalWrite(ENABLE_PIN_L, HIGH); //enable the half bridges
+      digitalWrite(ENABLE_PIN_R, HIGH);
+    }
   }
 }
 
